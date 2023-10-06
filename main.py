dd61ecbe072c99ae3c70292fa86121ea835d9f59
Giuseppe Scarlato <giuseppe499@live.it>
2023-10-06 22:34:02 +0200

    Added license

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+"""
+NPD implementation
+
+Copyright (C) 2023 Giuseppe Scarlato
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Giuseppe Scarlato"
+__contact__ = "giuseppe499[at]live.com"
+__copyright__ = "Copyright 2023, Giuseppe Scarlato"
+__date__ = "2023/10/06"
+__license__ = "GPLv3"
+
 from PIL import Image
 import numpy as np
 import scipy as sp
