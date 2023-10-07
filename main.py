ea6835416be5dffb9dfecbd1943e08b58001e404
Giuseppe Scarlato <giuseppe499@live.it>
2023-10-07 11:31:03 +0200

    Added deconvolution functions

--- main.py
+++ main.py
@@ -54,22 +54,38 @@ def generatePsfMatrix(size: int, sigma: float) -> np.array:
 
     return psfMatrix
 
+def fftConvolve2D(in1, in2):
+    return np.real(np.fft.ifft2(np.fft.fft2(in1) * np.fft.fft2(in2)))
+
+def deConvolve2D(conv, psf, epsilon: float):
+    return np.real(np.fft.ifft2(np.fft.fft2(conv) / np.clip(np.fft.fft2(psf),epsilon, None) ))
+
+def deConvolve2DThikonov(conv, psf, alpha):
+    psfFFT = np.fft.fft2(psf)
+    psfFFTC = np.conjugate(psfFFT)
+    convFFT = np.fft.fft2(conv)
+    return np.real(np.fft.ifft2( psfFFTC*convFFT/(psfFFTC*psfFFT + alpha) ) )
 
 if __name__ == '__main__':
     # Load image
     image = Image.open(IMGPATH)
     image = np.asarray(image) / 255
+    n = image.shape[0];
     print(image.shape)
 
     # Generate PSF
-    psf = generatePsfMatrix(256, 16)
+    psf = generatePsfMatrix(n, 8)
+    # Center PSF
+    psf = np.roll(psf, (-psf.shape[0] // 2, -psf.shape[0] // 2), axis=(0, 1))
     # Generate noise
-    noise = np.random.normal(size = image.shape, scale = 1e-2)
+    noise = np.random.normal(size = image.shape, scale = 1e-4)
     # Generate blurred image
-    conv = sp.signal.convolve(image, psf, "same")
+    conv = fftConvolve2D(image, psf)
 
     # Add noise to blurred image
-    b = conv + noise
+    b = np.clip(conv + noise,0,1)
+    imRec = deConvolve2D(b, psf, 1e-3)
+    imRecThikonov = deConvolve2DThikonov(b, psf, 1e-6)
 
     # Show PSF
     #plt.imshow(psf)
@@ -84,5 +100,9 @@ if __name__ == '__main__':
     f, axs = plt.subplots(1,2)
     axs[0].imshow(image, cmap="gray", vmin=0, vmax=1)
     axs[1].imshow(b, cmap="gray", vmin=0, vmax=1)
+    f, axs = plt.subplots(1, 3)
+    axs[0].imshow(image, cmap="gray", vmin=0, vmax=1)
+    axs[1].imshow(imRec, cmap="gray", vmin=0, vmax=1)
+    axs[2].imshow(imRecThikonov, cmap="gray", vmin=0, vmax=1)
 
     plt.show()
