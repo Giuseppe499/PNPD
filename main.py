f651e311f2cc40ab1b42d121787c7b58fc423750
Giuseppe Scarlato <giuseppe499@live.it>
2023-10-11 19:03:20 +0200

    Refactoring

--- main.py
+++ main.py
@@ -29,55 +29,61 @@ from numpy.linalg import norm
 from numpy.fft import fft2
 from mathExtras import (sInner, softTreshold, gradLeastSquares, fftConvolve2D,
                         generatePsfMatrix)
-from solvers import deConvolve2D, deConvolve2DThikonov, FFBS
+from solvers import deConvolve2D, deConvolve2DThikonov, FFBS, FBS
 from matplotlib import pyplot as plt
 
 IMGPATH = "cameraman.tif"
+
+np.random.seed = 42
 
 if __name__ == '__main__':
     # Load image
     image = Image.open(IMGPATH)
     image = np.asarray(image) / 255
+    image = image[::2, ::2]
     n = image.shape[0]
     print(image.shape)
 
     # Generate PSF
     psf = generatePsfMatrix(n, 8)
+    # plt.imshow(psf)
+    # plt.show()
     # Center PSF
     psf = np.roll(psf, (-psf.shape[0] // 2, -psf.shape[0] // 2), axis=(0, 1))
     # Generate noise
-    noise = np.random.normal(size=image.shape, scale=1e-4)
+    noise = np.random.normal(size=image.shape)
+    noise *= 0.1 * norm(image) / norm(noise)
     # Generate blurred image
     conv = fftConvolve2D(image, psf)
 
     # Add noise to blurred image
     b = np.clip(conv + noise, 0, 1)
     # Simple treshold deblur
-    imRec = deConvolve2D(b, psf, 1e-3)
+    imRec = deConvolve2D(b, psf, 1e-1)
     # Thikonov deblur
-    imRecThikonov = deConvolve2DThikonov(b, psf, 1e-7)
+    imRecThikonov = deConvolve2DThikonov(b, psf, 1e-3)
 
     # FISTA deblur
     bFFT = fft2(b)
     psfFFT = fft2(psf)
     psfFFTC = np.conjugate(psfFFT)
-    imRecThikonov1 = FFBS(imRecThikonov,
-                          gradf=lambda x: gradLeastSquares(x, bFFT, psfFFT,
-                                                           psfFFTC),
-                          proxg=lambda alpha, x: softTreshold(alpha * 0, x),
-                          f=lambda x: sInner(
-                              (fftConvolve2D(x, psf) - b).ravel()),
-                          g=lambda x: 0 * norm(x),
-                          stepSize=.6, maxit=int(1e3), tol=1e-16)
+    imRecThikonov1 = FBS(b,
+                         gradf=lambda x: gradLeastSquares(x, bFFT, psfFFT,
+                                                          psfFFTC),
+                         proxg=lambda alpha, x: softTreshold(alpha * 0, x),
+                         f=lambda x: sInner(
+                             (fftConvolve2D(x, psf) - b).ravel()),
+                         g=lambda x: 0 * norm(x),
+                         stepSize=1, maxit=int(1e3), tol=1e-4, xOrig=image)
 
     # Show PSF
     # plt.imshow(psf)
 
     # Show blurred image, noise and the sum of these
-    f, axs = plt.subplots(1, 3)
-    axs[0].imshow(conv, cmap="gray", vmin=0, vmax=1)
-    axs[1].imshow(noise, cmap="gray")
-    axs[2].imshow(b, cmap="gray", vmin=0, vmax=1)
+    # f, axs = plt.subplots(1, 3)
+    # axs[0].imshow(conv, cmap="gray", vmin=0, vmax=1)
+    # axs[1].imshow(noise, cmap="gray")
+    # axs[2].imshow(b, cmap="gray", vmin=0, vmax=1)
 
     # Show original image and blurred image
     f, axs = plt.subplots(1, 2)
