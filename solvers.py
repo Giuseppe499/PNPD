231f618d6cdff450796ddfe47a2289c2ed6a7361
Giuseppe Scarlato <giuseppe499@live.it>
2023-10-11 19:02:44 +0200

    Refactoring

--- solvers.py
+++ solvers.py
@@ -37,90 +37,60 @@ def deConvolve2DThikonov(conv, psf, alpha):
 
 
 def FBS(x0, gradf: Callable, proxg: Callable, f: Callable, g: Callable,
-        stepSize: float, tol: float = 1e-4, maxit: int = 100):
+        stepSize: float, xOrig, tol: float = 1e-4, maxit: int = 100):
     """
     Forward-Backward Splitting
     Approximate argmin_{x \in R^d} f(x) + g(x) where f is differentiable and
     the proximity operator of g is known.
     """
-    for i in range(maxit):
-        x = proxg(stepSize, x0 - stepSize * gradf(x0))
-        print(norm(x - x0))
-        if norm(x - x0) / norm(x) < tol:
-            print(i)
-            print(norm(x - x0))
-            return x
-        xlast = x0
-        x0 = x
-    print(norm(x - xlast))
-    print("Exceded maxit")
-    return x
-
-
-def FBSB(x0, gradf: Callable, proxg: Callable, f: Callable, g: Callable,
-         stepSize: float, rho: float = .5, tol: float = 1e-4, maxit: int = 100):
-    """
-    Forward-Backward Splitting with Backtracking
-    Approximate argmin_{x \in R^d} f(x) + g(x) where f is differentiable and
-    the proximity operator of g is known.
-    """
     val0 = f(x0) + g(x0)
-    gradfx0 = gradf(x0);
     for i in range(maxit):
-        stepSize = stepSize
-        x = proxg(stepSize, x0 - stepSize * gradfx0)
-        val = f(x) + g(x)
-        while val > val0 - 1e-4 * stepSize * sInner(gradfx0.ravel()):
-            stepSize = rho * stepSize
-            x = proxg(stepSize, x0 - stepSize * gradfx0)
-            val = f(x) + g(x)
-            if stepSize < 1e-16:
-                print("stepSize too short")
-                return x
-        print(stepSize)
-        print(val)
-        if val0 - val < tol:
-            print(i)
-            print(val - val0)
-            return x
-        # plt.imshow(x, cmap="gray", vmin=0, vmax=1)
-        # plt.draw()
-        # plt.pause(0.001)
-        x0 = x
-        val0 = val
-        gradfx0 = gradf(x0);
-    print(val)
+        x1 = proxg(stepSize, x0 - stepSize * gradf(x0))
+        val1 = f(x1) + g(x1)
+        print("Iteration: " + str(i), end="")
+        print(", RRE: " + str(norm(xOrig - x1) / norm(xOrig)), end="")
+        print(", f(x1) + g(x1): " + str(val1), end="")
+        print(", delta val: " + str(val1 - val1))
+        if val0 - val1 < tol:
+            print(val0 - val1)
+            return x1
+        xlast = x0
+        x0 = x1
     print("Exceded maxit")
-    return x
+    return x1
 
 
 def FFBS(x0, gradf: Callable, proxg: Callable, f: Callable, g: Callable,
-         stepSize: float, t0: float = 1, tol: float = 1e-4, maxit: int = 100):
+         stepSize: float, xOrig, t0: float = 1, tol: float = 1e-4, maxit: int
+         = 100):
     """
     Fast Forward-Backward Splitting (FISTA-like algorithm)
     Approximate argmin_{x \in R^d} f(x) + g(x) where f is differentiable and
     the proximity operator of g is known.
     """
-    xLast = x0
-    x0 = proxg(stepSize, xLast - stepSize * gradf(xLast))
-    val0 = f(x0) + g(x0)
+    rreList = []
+    x1 = proxg(stepSize, x0 - stepSize * gradf(x0))
+    val1 = f(x1) + g(x1)
     for i in range(maxit):
         t = .5 + .5 * np.sqrt(1 + 4 * t0 * t0)
-        y = x0 + (t0 - 1) / t * (x0 - xLast)
-        x = proxg(stepSize, y - stepSize * gradf(y))
-        val = f(x) + g(x)
-        print(val0 - val)
-        if np.abs(val0 - val) < tol:
-            print(i)
-            print(val0 - val)
-            return x
+        y = x1 + (t0 - 1) / t * (x1 - x0)
+        x2 = proxg(stepSize, y - stepSize * gradf(y))
+        val2 = f(x2) + g(x2)
+        rre = str(norm(xOrig - x2))
+        rreList.append(rre)
+        print("Iteration: " + str(i), end="")
+        print(", RRE: " + rre, end="")
+        print(", f(x1) + g(x1): " + str(val1), end="")
+        print(", delta val: " + str(val1 - val1))
+
+        if np.abs(val1 - val2) < tol:
+            break
         # plt.imshow(x, cmap="gray", vmin=0, vmax=1)
         # plt.draw()
         # plt.pause(0.001)
-        xLast = x0
-        x0 = x
-        val0 = val
+        x0 = x1
+        x1 = x2
+        val1 = val2
         t0 = t
-    print(val)
     print("Exceded maxit")
-    return x
+    return x2, rreList
