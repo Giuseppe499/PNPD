8d05eb309b3cf6c4fa31529f686db3f1ae3a7f8e
Giuseppe Scarlato <giuseppe499@live.it>
2024-01-27 12:43:03 +0100

    Add NPD_step and NPDIT_step functions

--- solvers.py
+++ solvers.py
@@ -41,90 +41,132 @@ def deConvolve2DThikonovPlusEstimate(conv, psf, estimate, alpha):
     convFFT = fft2(conv)
     return np.real(ifft2((psfFFTC * convFFT + alpha*estimateFFT) / (psfFFTC * psfFFT + alpha)))
 
+def NPD_step(x0, x1, y0, gradf: Callable, proxhs: Callable, mulW: Callable,
+             mulWT: Callable, pStep: float, dStep: float, t0: float, C: float,
+             rho_i: float, kMax: int = 1):
+    t = .5 + .5 * np.sqrt(1 + 4 * t0 * t0)
+    gamma = (t0 - 1) / t
+    gamma = min(gamma, C*rho_i / (norm(x1 - x0)))
+    xBar = x1 + gamma * (x1 - x0)
+    # Primal Dual Iteration
+    #k = 0
+    x2 = xBar - pStep * gradf(xBar) - pStep * mulWT(y0)
+    y1 = proxhs(dStep / pStep, y0 + (dStep / pStep) * mulW(x2))
+    y0 = y1
+    x1Sum = np.zeros(x1.shape)
+    for k in range(1,kMax):
+        x2 = xBar - pStep * gradf(xBar) - pStep * mulWT(y0)
+        x1Sum += x2
+        y1 = proxhs(dStep / pStep, y0 + (dStep / pStep) * mulW(x2))
+        y0 = y1
+    x2 = xBar - pStep * gradf(xBar) - pStep * mulWT(y0)
+    x1Sum += x2
+    x2 = x1Sum / kMax
+    return x1, x2, t, y1
 
 def NPD(x0, gradf: Callable, proxhs: Callable, mulW: Callable, mulWT: Callable,
         f: Callable, h: Callable, pStep: float, dStep: float, xOrig,
-        kMax: int = 1, t0: float = 0, tol: float = 1e-4, dp: float = 1, maxit: int = 100):
+        kMax: int = 1, t0: float = 0, rho: Callable = lambda i: 1/(i+1)**1.1,
+        tol: float = 1e-4, dp: float = 1, maxit: int = 100):
     """
     Nested Primal Dual (FISTA-like algorithm)
     Approximate argmin_{x \in R^d} f(x) + h(Wx) where f is differentiable and
     the proximity operator of h* (Fenchel conjugate of h) is known.
     """
     rreList = []
-    x1 = x0
-    val1 = f(x1) + h(mulW(x1))
-    y0 = proxhs(dStep / pStep, dStep / pStep * mulW(x1))
-    for i in range(maxit):
-        t = .5 + .5 * np.sqrt(1 + 4 * t0 * t0)
-        xBar = x1 + (t0 - 1) / t * (x1 - x0)
-        # Primal Dual Iteration
-        x1Sum = np.zeros(x1.shape)
-        for k in range(kMax):
-            x2 = xBar - pStep * gradf(xBar) - pStep * mulWT(y0)
-            x1Sum += x2
-            y1 = proxhs(dStep / pStep, y0 + (dStep / pStep) * mulW(x2))
-            y0 = y1
-        x2 = xBar - pStep * gradf(xBar) - pStep * mulWT(y0)
+    y0 = np.zeros(proxhs(1, mulW(x0)).shape)
+
+    i = 0
+    x0, x1, t0, y0 = NPD_step(x0=x0, x1=x0, y0=y0, gradf=gradf, proxhs=proxhs,
+                                  mulW=mulW, mulWT=mulWT, pStep=pStep, dStep=dStep,
+                                  t0=t0, C=0, rho_i=1, kMax=kMax)
+    rre = norm(xOrig - x1) / norm(xOrig)
+    rreList.append(rre)
+    print("Iteration: " + str(i), end="")
+    print(", RRE: " + str(rre))
+    if f(x1) < dp*tol:
+        print("tol reached")
+    else:
+        C = 10 * norm(x1 - x0) 
+        for i in range(1,maxit):
+            x0, x1, t0, y0 = NPD_step(x0=x0, x1=x1, y0=y0, gradf=gradf, proxhs=proxhs,
+                                    mulW=mulW, mulWT=mulWT, pStep=pStep, dStep=dStep,
+                                    t0=t0, C=C, rho_i=rho(i), kMax=kMax)
+            val2 = f(x1) + h(mulW(x1))
+            rre = norm(xOrig - x1) / norm(xOrig)
+            rreList.append(rre)
+            print("Iteration: " + str(i), end="")
+            print(", RRE: " + str(rre), end="")
+            print(", f(x1) + g(x1): " + str(val2))
+            if f(x1) < dp*tol:
+                print("tol reached")
+                break
+    return x1, rreList
+
+def NPDIT_step(x0, x1, y0, gradf: Callable, proxhs: Callable, mulW: Callable,
+               mulWT: Callable, mulPIn: Callable, pStep: float, dStep: float,
+               PReg: float, t0: float, C: float, rho_i: float, kMax: int = 1):
+    t = .5 + .5 * np.sqrt(1 + 4 * t0 * t0)
+    gamma = (t0 - 1) / t
+    gamma = min(gamma, C*rho_i / (norm(x1 - x0)))
+    xBar = x1 + gamma * (x1 - x0)
+    # Primal Dual Iteration
+    #k = 0
+    x2 = xBar - pStep * mulPIn(PReg, gradf(xBar)) - pStep * mulWT(y0)
+    y1 = proxhs(dStep / pStep, y0 + (dStep / pStep) * mulW(x2))
+    y0 = y1
+    x1Sum = np.zeros(x1.shape)
+    for k in range(1,kMax):
+        x2 = xBar - pStep * mulPIn(PReg, gradf(xBar)) - pStep * mulWT(y0)
         x1Sum += x2
-        x2 = x1Sum / (kMax + 1)
-        val2 = f(x2) + h(mulW(x2))
-        rre = norm(xOrig - x2) / norm(xOrig)
-        rreList.append(rre)
-        print("Iteration: " + str(i), end="")
-        print(", RRE: " + str(rre), end="")
-        print(", f(x1) + g(x1): " + str(val2), end="")
-        print(", f(x2): " + str(f(x2)))
-        if f(x2) < dp*tol:
-            print("tol reached")
-            break
-        x0 = x1
-        x1 = x2
-        val1 = val2
-        t0 = t
-    return x2, rreList
+        y1 = proxhs(dStep / pStep, y0 + (dStep / pStep) * mulW(x2))
+        y0 = y1
+    x2 = xBar - pStep * mulPIn(PReg, gradf(xBar)) - pStep * mulWT(y0)
+    x1Sum += x2
+    x2 = x1Sum / kMax
+    return x1, x2, t, y1
 
 
 def NPDIT(x0, gradf: Callable, proxhs: Callable, mulW: Callable,
           mulWT: Callable, mulPIn: Callable, f: Callable, h: Callable,
           pStep: float, dStep: float, PReg: float, xOrig, kMax: int = 1,
-          t0: float = 0, tol: float = 1e-4, dp: float = 1, maxit: int = 100):
+          t0: float = 0, rho: Callable = lambda i: 1/(i+1)**1.1, tol: float = 1e-4, dp: float = 1, maxit: int = 100):
     """
     Nested Primal Dual (FISTA-like algorithm)
     Approximate argmin_{x \in R^d} f(x) + h(Wx) where f is differentiable and
     the proximity operator of h* (Fenchel conjugate of h) is known.
     """
     rreList = []
-    x1 = x0
-    val1 = f(x1) + h(mulW(x1))
-    y0 = proxhs(dStep / pStep, dStep / pStep * mulW(x1))
-    for i in range(maxit):
-        t = .5 + .5 * np.sqrt(1 + 4 * t0 * t0)
-        xBar = x1 + (t0 - 1) / t * (x1 - x0)
-        # Primal Dual Iteration
-        x1Sum = np.zeros(x1.shape)
-        for k in range(kMax):
-            x2 = xBar - pStep * mulPIn(PReg, gradf(xBar)) - pStep * mulWT(y0)
-            x1Sum += x2
-            y1 = proxhs(dStep / pStep, y0 + (dStep / pStep) * mulW(x2))
-            y0 = y1
-        x2 = xBar - pStep * mulPIn(PReg, gradf(xBar)) - pStep * mulWT(y0)
-        x1Sum += x2
-        x2 = x1Sum / (kMax + 1)
-        val2 = f(x2) + h(mulW(x2))
-        rre = norm(xOrig - x2) / norm(xOrig)
-        rreList.append(rre)
-        print("Iteration: " + str(i), end="")
-        print(", RRE: " + str(rre), end="")
-        print(", f(x1) + g(x1): " + str(val2), end="")
-        print(", f(x2): " + str(f(x2)))
-        if f(x2) < dp*tol:
-            print("tol reached")
-            break
-        x0 = x1
-        x1 = x2
-        val1 = val2
-        t0 = t
-    return x2, rreList
+    y0 = np.zeros(proxhs(1, mulW(x0)).shape)
+
+    i = 0
+    x0, x1, t0, y0 = NPDIT_step(x0=x0, x1=x0, y0=y0, gradf=gradf, proxhs=proxhs,
+                                  mulW=mulW, mulWT=mulWT, mulPIn=mulPIn, pStep=pStep,
+                                  dStep=dStep, PReg=PReg, t0=t0, C=0, rho_i=1,
+                                  kMax=kMax)
+    rre = norm(xOrig - x1) / norm(xOrig)
+    rreList.append(rre)
+    print("Iteration: " + str(i), end="")
+    print(", RRE: " + str(rre))
+    if f(x1) < dp*tol:
+        print("tol reached")
+    else:
+        C = 10 * norm(x1 - x0) 
+        for i in range(1,maxit):
+            x0, x1, t0, y0 = NPDIT_step(x0=x0, x1=x1, y0=y0, gradf=gradf, proxhs=proxhs,
+                                  mulW=mulW, mulWT=mulWT, mulPIn=mulPIn, pStep=pStep,
+                                  dStep=dStep, PReg=PReg, t0=t0, C=C, rho_i=rho(i),
+                                  kMax=kMax)
+            val2 = f(x1) + h(mulW(x1))
+            rre = norm(xOrig - x1) / norm(xOrig)
+            rreList.append(rre)
+            print("Iteration: " + str(i), end="")
+            print(", RRE: " + str(rre), end="")
+            print(", f(x1) + g(x1): " + str(val2))
+            if f(x1) < dp*tol:
+                print("tol reached")
+                break
+    return x1, rreList
 
 import torch
 def torch_NPDIT_step(x0, x1, y0, gradf: Callable, proxhs: Callable, mulW: Callable,
