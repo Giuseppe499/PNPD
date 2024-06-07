538adb0899426ab5fe310c324d826580bb3465e8
Giuseppe Scarlato <giuseppe499@live.it>
2024-06-07 09:31:37 +0200

    Refactor

--- tests/example_1/PNPD_comparison.py
+++ tests/example_1/PNPD_comparison.py
@@ -1,4 +1,9 @@
 import tests.PNPD_comparison as test
+from init import DATA_SAVE_PATH as EXAMPLE_DATA_PATH
+from init import EXAMPLE_NAME
+from tests.constants import *
+
+SAVE_PATH = "." + PICKLE_SAVE_FOLDER + "/" + EXAMPLE_NAME + "/" + test.TEST_NAME
 
 parameters = test.Parameters(nu=1e-1, lam_PNPD=2e-3, lam_NPD=2e-4, iterations=150, k_max= [3,1,3,3])
 
@@ -9,10 +14,9 @@ def plot(*args, **kwargs):
     return test.plot(*args, **kwargs)
 
 if __name__ == "__main__":
-    from init import DATA_SAVE_PATH as EXAMPLE_DATA_PATH
-    from init import EXAMPLE_NAME
     from utilities import load_data
-    from tests.constants import *
+    
+    PICKLE_SAVE_FOLDER + "/" + EXAMPLE_NAME + "/" + test.TEST_NAME
     data = load_data(EXAMPLE_DATA_PATH)
-    test_data = compute(data)
+    test_data = compute(data=data, save_path=SAVE_PATH)
     plot(test_data, ".." + PLOTS_SAVE_FOLDER + "/" + EXAMPLE_NAME + "/" + test.TEST_NAME + "/")
